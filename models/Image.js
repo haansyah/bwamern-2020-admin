8608e428d469a1c7accebd01d6dd372db501397f
Ari Sutarman <hansyah242@gmail.com>
2022-08-15 15:57:38 +0700

    CRUD BANK, Category

---
+++ models/Image.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const imageSchema = new mongoose.Schema({
+  imageUrl: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Image", imageSchema);
